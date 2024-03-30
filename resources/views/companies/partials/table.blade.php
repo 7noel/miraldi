878c376fd130ae5a9d5627481ee1996eb5ab55b0
7noel <noel.logan@gmail.com>
2024-03-29 21:37:14 -0500

    111111

--- resources/views/companies/partials/table.blade.php
+++ resources/views/companies/partials/table.blade.php
@@ -1,10 +1,11 @@
+<div class="table-responsive">
 <table class="{{ config('options.styles.table') }}">
 	<thead class="{{ config('options.styles.thead') }}">
 		<tr>
-			<th>Razón Social</th>
-			<th>DNI/RUC</th>
-			<th>Creación</th>
-			<th>Modificación</th>
+			<th style="min-width: 250px">Razón Social</th>
+			<th class="text-center">DNI/RUC</th>
+			<th class="text-center" style="min-width: 100px">Creación</th>
+			<th class="text-center" style="min-width: 100px">Modificación</th>
 			<th>Acciones</th>
 		</tr>
 	</thead>
@@ -12,10 +13,10 @@
 		@foreach($models as $model)
 		<tr data-id="{{ $model->id }}">
 			<td>{{ $model->CNOMCLI }}</td>
-			<td>{{ $model->CCODCLI }}</td>
-			<td>{{ $model->DFECINS }}</td>
-			<td>{{ $model->DFECMOD }}</td>
-			<td>
+			<td class="text-center">{{ $model->CCODCLI }}</td>
+			<td class="text-center">{{ $model->DFECINS }}</td>
+			<td class="text-center">{{ $model->DFECMOD }}</td>
+			<td style="white-space: nowrap;">
 				<a href="{{ route($routes['show'], $model) }}" class="btn btn-outline-secondary btn-sm" title="Ver">{!! $icons['view'] !!}</a>
 				<a href="{{ route( $routes['edit'] , $model) }}" class="btn btn-outline-primary btn-sm" title="Editar">{!! $icons['edit'] !!}</a>
 				{{-- <a href="#" class="btn-delete btn btn-outline-danger btn-sm" title="Eliminar">{!! $icons['remove'] !!}</a> --}}
@@ -24,3 +25,4 @@
 		@endforeach
 	</tbody>
 </table>
+</div>
